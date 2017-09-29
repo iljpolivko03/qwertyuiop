c0aeabd8be4e46ee28bd2e77ba97f84b5914f7a7
iljpolivko03 <31990232+iljpolivko03@users.noreply.github.com>
2017-09-29 15:36:58 +0300

    qwe

--- index.php
+++ index.php
@@ -3,4 +3,6 @@ echo "O privet";
 require 'vendor/autoload.php';
 $app = new \atk4\ui\App("A ja tebja znaju");
 $app->initLayout("Centered");
-
+$button = $app->add('Button');
+$button->set('Add invoice');
+$button->icon = 'plus';
