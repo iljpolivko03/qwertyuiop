ad6d26d8a9c3579d4ae807210ae573bcb4041209
vikanica <32392416+vikanica@users.noreply.github.com>
2017-10-13 15:05:50 +0300

    qwe

--- index.php
+++ index.php
@@ -3,15 +3,20 @@ echo "O privet";
 require 'vendor/autoload.php';
 $app = new \atk4\ui\App("A ja tebja znaju");
 $app->initLayout("Centered");
+/*
 $button = $app->add('Button');
 $button->set('Add invoice');
 $button->icon = 'plus';
 $button->link('https://www.youtube.com/');
+
 $button = $app->add('Button');
 $button->set('Add invoice');
 $button->icon = 'plus';
 $button->link('https://www.facebook.com/');
+
 $button = $app->add('Button');
 $button->set('Add invoice');
 $button->icon = 'plus';
 $button->link('https://www.e-klase.lv/lv/');
+*/
+$button= $app->add(['Button','Ilja']);
---
+++ index2.php
@@ -0,0 +1,5 @@
+<?php
+echo "O privet";
+require 'vendor/autoload.php';
+$app = new \atk4\ui\App("A ja tebja znaju");
+$app->initLayout("Centered");
