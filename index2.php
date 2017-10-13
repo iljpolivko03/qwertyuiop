517be2a1774bdc7061f673fe2cf37b30b1b40a95
vikanica <32392416+vikanica@users.noreply.github.com>
2017-10-13 15:33:22 +0300

    QWE

--- index2.php
+++ index2.php
@@ -4,3 +4,6 @@ require 'vendor/autoload.php';
 $app = new \atk4\ui\App("Добро пожаловать");
 $app->initLayout("Centered");
 $label=$app->add(['Label','Поиск']);
+$button= $app->add(['Button','Ilja']);
+$button->link('index.php');
+$button->addClass('Massive Blue');
