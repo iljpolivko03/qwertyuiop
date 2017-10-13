e68729a2214efaa4a730a87a29cf4569a0b6c3f2
vikanica <32392416+vikanica@users.noreply.github.com>
2017-10-13 15:39:55 +0300

    qwe

--- index2.php
+++ index2.php
@@ -3,7 +3,8 @@ echo "O privet";
 require 'vendor/autoload.php';
 $app = new \atk4\ui\App("Добро пожаловать");
 $app->initLayout("Centered");
-$label=$app->add(['Label','Поиск']);
+$label=$app->add(['Label','Поиск','detail']);
+
 $button= $app->add(['Button','Ilja']);
 $button->link('index.php');
 $button->addClass('Massive Blue');
