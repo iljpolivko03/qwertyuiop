5c262842f0d76c2bbc99ed0d88ac7e52bb4b05f7
vikanica <32392416+vikanica@users.noreply.github.com>
2017-10-20 15:05:24 +0300

    qwe

--- index.php
+++ index.php
@@ -5,18 +5,19 @@ $app = new \atk4\ui\App("Добро пожаловать");
 $app->initLayout("Centered");
 
 $button = $app->add('Button');
-$button->set('Add invoice');
-$button->icon = 'plus';
+$button->set('YouTube');
+$button->icon = 'Image';
 $button->link('https://www.youtube.com/');
 
 $button = $app->add('Button');
-$button->set('Add invoice');
+$button->set('Facebook');
 $button->icon = 'Comments';
+$button->addClass('massive blue');
 $button->link('https://www.facebook.com/');
 
 $button = $app->add('Button');
-$button->set('Add invoice');
-$button->icon = 'plus';
+$button->set('Aliexpress');
+$button->icon = 'Shopping Bag';
 $button->link('https://www.aliexpress.com');
 
 $button= $app->add(['Button','Ilja']);
@@ -25,3 +26,10 @@ $button->addClass('Massive Blue');
 
 $text=$app->add(['Text','Here can be text']);
 $text->addParagraph('Text');
+
+$a= 3;
+  if ($a>0) {
+    echo'Верно!';
+  }else{
+    echo 'Неверно!';
+  }
