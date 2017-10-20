03ee52f6e79fd85a4d673bb1e39e5c8ab13b54b3
vikanica <32392416+vikanica@users.noreply.github.com>
2017-10-20 14:40:57 +0300

    qwe

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ echo "O privet";
 require 'vendor/autoload.php';
 $app = new \atk4\ui\App("Добро пожаловать");
 $app->initLayout("Centered");
-/*
+
 $button = $app->add('Button');
 $button->set('Add invoice');
 $button->icon = 'plus';
@@ -18,7 +18,7 @@ $button = $app->add('Button');
 $button->set('Add invoice');
 $button->icon = 'plus';
 $button->link('https://www.e-klase.lv/lv/');
-*/
+
 $button= $app->add(['Button','Ilja']);
 $button->link('index2.php');
 $button->addClass('Massive Blue');
