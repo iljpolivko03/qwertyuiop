68c8670265ca3211cc71362d293d1b478c4f9e44
vikanica <32392416+vikanica@users.noreply.github.com>
2017-10-20 15:10:46 +0300

    qwe

--- index.php
+++ index.php
@@ -7,17 +7,19 @@ $app->initLayout("Centered");
 $button = $app->add('Button');
 $button->set('YouTube');
 $button->icon = 'Image';
+$button->addClass('medium red');
 $button->link('https://www.youtube.com/');
 
 $button = $app->add('Button');
 $button->set('Facebook');
 $button->icon = 'Comments';
-$button->addClass('massive blue');
+$button->addClass('medium blue');
 $button->link('https://www.facebook.com/');
 
 $button = $app->add('Button');
 $button->set('Aliexpress');
 $button->icon = 'Shopping Bag';
+$button->addClass('medium orange');
 $button->link('https://www.aliexpress.com');
 
 $button= $app->add(['Button','Ilja']);
