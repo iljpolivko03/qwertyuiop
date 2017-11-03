df9df44e9d3ea5acb66b68c44cd9bf0907dd14e5
vikanica <32392416+vikanica@users.noreply.github.com>
2017-11-03 15:50:16 +0200

    q

--- index.php
+++ index.php
@@ -116,4 +116,4 @@ $label->addClass($color);
 
 $button=$app->add('Button');
 $button->set('КНОПКА');
-$button->link(['index3','name'=>'Ilja','name'=>'Polivko']);
+$button->link(['index3','name'=>'Ilja','surname'=>'Polivko']);
