053d227c1690d216b1294e83e0b35f5687c3db8d
vikanica <32392416+vikanica@users.noreply.github.com>
2017-11-03 15:37:36 +0200

    q

--- index3.php
+++ index3.php
@@ -4,5 +4,4 @@ $app = new \atk4\ui\App("Добро пожаловать");
 $app->initLayout("Centered");
 
 $c=$_GET['name'];
-$label=$app->(['Label',$c]);
-$view=$app->layout->add(['View','index3'])
+$label=$app->add(['Label',$c]);
