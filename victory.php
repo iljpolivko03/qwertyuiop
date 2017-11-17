337cac23cfeec9c5c543a54c074abcfd62c6bb7c
vikanica <32392416+vikanica@users.noreply.github.com>
2017-11-17 15:04:36 +0200

    q

--- victory.php
+++ victory.php
@@ -3,3 +3,8 @@ echo "Izi izi izi ril top";
 require'vendor/autoload.php';
 $app = new \atk4\ui\App("Я угадал твоё число!");
 $app->initLayout("Centered");
+
+$button=$app->add(['Button']);
+$button->set('Ещё раз');
+$button-link(['index']);
+$button->addClass('green');
