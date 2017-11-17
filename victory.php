e0707ebd2be4e3fac2171919fa6f8426fe702cda
vikanica <32392416+vikanica@users.noreply.github.com>
2017-11-17 15:07:26 +0200

    q

--- victory.php
+++ victory.php
@@ -5,6 +5,7 @@ $app = new \atk4\ui\App("Я угадал твоё число!");
 $app->initLayout("Centered");
 
 $button=$app->add(['Button']);
+$app->initLayout("Centered");
 $button->set('Ещё раз');
 $button->link(['index']);
 $button->addClass('green');
